2abf90ab7e713bb132bdee333fe0531de810f529
CrazyIvanovs <lastovskis08@gmail.com>
2025-06-05 19:30:26 +0300

    Created register method with its full functionality

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Hash;
 
 class AuthController extends Controller
 {
@@ -13,6 +15,21 @@ class AuthController extends Controller
             "email" => "required|email|unique:users|max:255",
             "password" => "required|confirmed|min:8|max:255"
         ]);
+
+        $hashedPassword = Hash::make($request->input("password"));
+
+        $user = User::create([
+            "name" => $request->input("name"),
+            "email" => $request->input("email"),
+            "password" => $hashedPassword
+        ]);
+
+        $token = $user->createToken("token")->plainTextToken;
+
+        return response()->json([
+            "user" => $user,
+            "token" => $token
+        ], 200);
     }
 
     public function login(Request $request)
