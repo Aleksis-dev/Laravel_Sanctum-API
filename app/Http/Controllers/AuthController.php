d2f5d6626b95c6f457f9ffaf7a2b9620eaef5577
CrazyIvanovs <lastovskis08@gmail.com>
2025-06-05 19:21:26 +0300

    Created request validation rules for register method and login method

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,12 +8,19 @@ class AuthController extends Controller
 {
     public function register(Request $request)
     {
-        
+        $request->validate([
+            "name" => "required|max:255",
+            "email" => "required|email|unique:users|max:255",
+            "password" => "required|confirmed|min:8|max:255"
+        ]);
     }
 
     public function login(Request $request)
     {
-        
+        $request->validate([
+            "email" => "required|email|exists|max:255",
+            "password" => "required|min:8|max:255"
+        ]);
     }
 
     public function logout(Request $request)
